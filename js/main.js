fddbc398637a9c422e2550132feb5dd694122ac2
MathiasChirivi <chirivimathias3@gmail.com>
2023-05-10 16:54:01 +0200

    ese + bonus ma text-decoration: line-through sottolinea anche i false

--- js/main.js
+++ js/main.js
@@ -1,1 +1,48 @@
 console.log("Benvenuto User");
+
+const {createApp} = Vue
+createApp({
+    data(){
+        return{
+            inputError: false,
+            newTask: "",
+            taskList:[
+                {
+                    text:"Programmare Usando Vue.js",
+                    done:"true"
+                },{
+                    text:"Programmare Usando php",
+                    done:"false"
+                },{
+                    text:"Creare un index.html e uno style.css",
+                    done:"false"
+                }
+            ]
+        }
+    },
+    methods:{
+        lineThrough(index){
+            return (this.taskList[index].done) ? 'textTrue' : '';
+        },
+        addTask(){
+            if(this.newTask.length >= 2){
+                this.inputError = false;
+
+                let newTask = {
+                    text: this.newTask,
+                    done: false
+                };
+                this.taskList.unshift(newTask);
+                this.newTask = "";
+            } else {
+                this.inputError = true;
+            }
+            },
+        removeToDo(index){
+                this.taskList.splice(index,1);
+            },
+        getDoneLine(index){
+            this.taskList[index].done = !this.taskList[index].done;
+            },
+        }
+}).mount('#app')
